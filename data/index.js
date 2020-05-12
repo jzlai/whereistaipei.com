00702ff8e75b5c37cdac25b3aea79939086ec6f9
jzlai <jun-zhe.lai@spring-media.de>
2020-05-12 20:23:28 +0200

    init

---
+++ data/index.js
@@ -0,0 +1,96 @@
+export const data = [
+  { company_name: 'Singapore Airlines', industry: 'Airline', stance: 'China' },
+  { company_name: 'Malaysian Airlines', industry: 'Airline', stance: 'China' },
+  { company_name: 'Garuda Indonesia', industry: 'Airline', stance: 'China' },
+  { company_name: 'Vietnam Airlines', industry: 'Airline', stance: 'China' },
+  { company_name: 'Philippine Airlines', industry: 'Airline', stance: 'China' },
+  { company_name: 'Thai Airways', industry: 'Airline', stance: 'China' },
+  {
+    company_name: 'Jetstar (under Qantas)',
+    industry: 'Airline',
+    stance: 'China'
+  },
+  { company_name: 'Scoot', industry: 'Airline', stance: 'China' },
+  { company_name: 'Cathay Pacific', industry: 'Airline', stance: 'China' },
+  { company_name: 'Cebu Pacific Air', industry: 'Airline', stance: 'China' },
+  { company_name: 'British Airways', industry: 'Airline', stance: 'China' },
+  { company_name: 'Turkish Airlines', industry: 'Airline', stance: 'China' },
+  { company_name: 'Air Canada', industry: 'Airline', stance: 'China' },
+  { company_name: 'Eva Air', industry: 'Airline', stance: 'Taiwan' },
+  { company_name: 'Qantas', industry: 'Airline', stance: 'China' },
+  { company_name: 'China Airlines', industry: 'Airline', stance: 'Taiwan' },
+  { company_name: 'Air France', industry: 'Airline', stance: 'China' },
+  { company_name: 'KLM', industry: 'Airline', stance: 'China' },
+
+  //Automotive
+  { company_name: 'Citroen', industry: 'Automotive', stance: 'China' },
+  { company_name: 'Peugeot', industry: 'Automotive', stance: 'China' },
+  { company_name: 'Ford', industry: 'Automotive', stance: 'China' },
+  { company_name: 'Mercedes-Benz', industry: 'Automotive', stance: 'China' },
+  { company_name: 'BMW', industry: 'Automotive', stance: 'China' },
+  { company_name: 'Honda', industry: 'Automotive', stance: 'Chinese-Taipei' },
+  { company_name: 'Volvo', industry: 'Automotive', stance: 'China' },
+  { company_name: 'Hyundai', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Jaguar', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Kia', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Land Rover', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Mitsubishi', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Nissan', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Porsche', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Suzuki', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Volkswagen', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Rolls Royce', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Toyota', industry: 'Automotive', stance: 'Taiwan' },
+  { company_name: 'Subaru', industry: 'Automotive', stance: 'Taiwan-ROC' },
+  { company_name: 'Infinity', industry: 'Automotive', stance: 'Taiwan-ROC' },
+  { company_name: 'Mazda', industry: 'Automotive', stance: 'Taiwan-ROC' },
+  { company_name: 'Tesla', industry: 'Automotive', stance: 'Taiwan-ROC' },
+  { company_name: 'Lexus', industry: 'Automotive', stance: 'Taiwan' },
+
+  //Technology
+  { company_name: 'Nintendo', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Microsoft', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Nvidia', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Apple', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Steam', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Intel', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Logitech', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Samsung', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'LG', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Spotify', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Sony', industry: 'Technology', stance: 'Taiwan Region' },
+  { company_name: 'HTC', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Huawei', industry: 'Technology', stance: 'China' },
+  { company_name: 'Lenovo', industry: 'Technology', stance: 'Taiwan Region' },
+  { company_name: 'Dell', industry: 'Technology', stance: 'Taiwan' },
+  { company_name: 'Bose', industry: 'Technology', stance: 'Taiwan' },
+  {
+    company_name: 'Beats by Dre',
+    industry: 'Technology',
+    stance: 'Taiwan Region'
+  },
+
+  //Food and beverages
+  { company_name: 'Nestle', industry: 'Food and beverages', stance: 'Taiwan' },
+  {
+    company_name: 'Red Bull',
+    industry: 'Food and beverages',
+    stance: 'Taiwan'
+  },
+  {
+    company_name: 'Coca-Cola',
+    industry: 'Food and beverages',
+    stance: 'Taiwan'
+  },
+  {
+    company_name: 'McDonalds',
+    industry: 'Food and beverages',
+    stance: 'Taiwan'
+  },
+
+  //Fashion
+  { company_name: 'Uniqlo', industry: 'Fashion', stance: 'Taiwan' },
+  { company_name: 'Nike', industry: 'Fashion', stance: 'Taiwan' },
+  { company_name: 'Puma', industry: 'Fashion', stance: 'China' },
+  { company_name: 'Ralph Lauren', industry: 'Fashion', stance: 'Taiwan' }
+];
---
+++ pages/index.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <section class="hero is-fullheight has-bg-img" id="top">
+      <div class="hero-body background">
+        <div class="container has-text-centered">
+          <h1 class="title is-1 has-text-white">
+            Where is Taipei?
+          </h1>
+          <h3 class="subtitle is-3 has-text-white">
+            Taipei is the capital of Taiwan.
+          </h3>
+          <h6 class="subtitle is-6 has-text-white">
+            Scroll down for a list of companies and their views on this topic...
+          </h6>
+          <a href="#" v-scroll-to="'#table'">
+            <b-icon icon="chevron-down" size="is-large" type="is-white" />
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-fullheight" id="table">
+      <div class="hero-body items-top">
+        <div class="container has-text-centered">
+          <b-input
+            v-model="search"
+            placeholder="Search"
+            icon="magnify"
+            clearable
+            style="margin-bottom: 15px"
+          >
+          </b-input>
+          <b-table
+            :data="filteredData"
+            :columns="columns"
+            :default-sort="['company_name', 'asc']"
+            :paginated="true"
+            :striped="true"
+            :per-page="17"
+          >
+            <template slot="empty">
+              <section class="section">
+                <div class="content has-text-grey has-text-centered">
+                  <p>
+                    <b-icon icon="emoticon-sad" size="is-large"> </b-icon>
+                  </p>
+                  <p>Nothing here.</p>
+                </div>
+              </section>
+            </template>
+          </b-table>
+        </div>
+      </div>
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>whereistaipei.com</strong> by
+            <a href="https://junzhe.me">Jun-Zhe Lai</a> <br />
+            Contribute on
+            <a
+              href="https://github.com/jzlai/whereistaipei.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <b-icon icon="github" />
+            </a>
+          </p>
+        </div>
+      </footer>
+    </section>
+  </div>
+</template>
+
+<script>
+import { data } from '@/data';
+import VueScrollTo from 'vue-scrollto';
+
+export default {
+  data() {
+    return {
+      search: '',
+      data,
+      columns: [
+        {
+          field: 'company_name',
+          label: 'Company Name',
+          sortable: true
+        },
+        {
+          field: 'industry',
+          label: 'Industry',
+          sortable: true
+        },
+        {
+          field: 'stance',
+          label: 'Stance',
+          sortable: true
+        }
+      ]
+    };
+  },
+  computed: {
+    filteredData() {
+      return this.data.filter(entry => {
+        return entry.company_name
+          .toLowerCase()
+          .includes(this.search.toLowerCase());
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.has-bg-img {
+  background: url('../assets/taipei.jpeg');
+  background-position: center center;
+  background-attachment: fixed;
+}
+
+.background {
+  padding: 0;
+  background-color: rgba(0, 0, 0, 0.85);
+}
+
+.items-top {
+  align-items: normal !important;
+}
+</style>
